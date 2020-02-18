c686462750c879c64bf8a5f410a9650ef4e37b48
Jaan Tollander de Balsch <jaan.tollander@gmail.com>
2020-02-18 15:05:59 +0200

    Solve with Gurobi

--- examples/run.jl
+++ examples/run.jl
@@ -5,12 +5,14 @@ using EnergySystemModel
 
 @info "Loading parameters"
 parameters = load_parameters("instance")
-specs = Specs(true, true, true, true)
+specs = Specs(true, true, false, false)
 
 @info "Creating the energy system model"
 model = energy_system_model(parameters, specs)
 
-# @info "Optimizing the model"
+@info "Optimizing the model"
 # using GLPK
-# optimizer = with_optimizer(GLPK.Optimizer)
-# optimize!(model, optimizer)
+# optimizer = with_optimizer(GLPK.Optimizer, tm_lim = 60000)
+using Gurobi
+optimizer = with_optimizer(Gurobi.Optimizer, TimeLimit=5*60)
+optimize!(model, optimizer)
