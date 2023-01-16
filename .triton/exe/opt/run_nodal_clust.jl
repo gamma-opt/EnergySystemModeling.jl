0fe815a39135a8ed9e0508d6d9846d8594c406fb
lucascondeixa <ldcondeixa@gmail.com>
2023-01-16 12:29:04 +0200

    Changes to run_nodal to accomodate "day"

--- .triton/exe/opt/run_nodal_clust.jl
+++ .triton/exe/opt/run_nodal_clust.jl
@@ -7,18 +7,18 @@ using Gurobi
 using Dates
 using DelimitedFiles
 
+# Define optimisation time limit
+opt_tlim = 60*60*72
+
 @info string("Starting instance ",ARGS[1]," @ ",now())
 
 # Instance including clustering info
 instance_clust = ARGS[1]                # Clustering instance with info
-clust_method = ARGS[2]                  # Clustering method ("","dc"/"dc_new"/"dc_nosun")
+clust_method = ARGS[2]                  # Clustering method ("","dc"/"dc_new"/"dc_nosun"/"day")
 nosun = parse(Bool,ARGS[3])             # Boolean regarding solar availability
 
 WRKDIR = "/scratch/work/condeil1/EnergySystemModeling.jl/examples"
 cd(WRKDIR)
-
-# Instance as a string(number)
-instance = parse(Int,instance_clust[9:12]) |> string
 
 @info "Loading parameters"
 constants_dir = "constants"
@@ -32,11 +32,20 @@ params_path_ftr = joinpath(instances_path,instance_ftr,".big_files","parameters"
 
 @info "Clustering parameters"
 aggregation_path = "/scratch/work/condeil1/EnergySystemModeling.jl/.triton/exe/aggregation/"
+
 # Defining the clustering results path
 if string(instance_clust[end-1]) == "m"
-        clust_method_path = joinpath(aggregation_path,string("aggreg_output_ed",clust_method),string("clust_out_ed",clust_method,".jld2"));
+        if clust_method == "day"
+                clust_method_path = joinpath(aggregation_path,string("aggreg_output_ed_",clust_method),string("clust_out_ed_",clust_method,".jld2"));
+        else
+                clust_method_path = joinpath(aggregation_path,string("aggreg_output_ed",clust_method),string("clust_out_ed",clust_method,".jld2"));
+        end
 elseif string(instance_clust[end-1]) == "w"
-        clust_method_path = joinpath(aggregation_path,string("aggreg_output_wd",clust_method),string("clust_out_wd",clust_method,".jld2"));
+        if clust_method == "day"
+                clust_method_path = joinpath(aggregation_path,string("aggreg_output_wd_",clust_method),string("clust_out_wd_",clust_method,".jld2"));
+        else
+                clust_method_path = joinpath(aggregation_path,string("aggreg_output_wd",clust_method),string("clust_out_wd",clust_method,".jld2"));
+        end
 end
 
 @info "Creating specifications"
@@ -53,10 +62,23 @@ specs = Specs(
         hydro_simple=false
 )
 
-@info "Adjusting parameters to instance"
+@info "Loading parameters to instance"
 # Updating the parameters to the clustering instance
 ParamsDict = load(joinpath(params_path_ftr,"parameters.jld2"));
-parameters = read_clust_instance(clust_method_path, instance, ParamsDict ; nosun=nosun);
+parameters = read_clust_instance(clust_method, clust_method_path, instance_clust, ParamsDict ; nosun=nosun);
+
+if clust_method == "day"
+        # Number of representative hours from the instance_clust
+        rep_hours = parse(Int,instance_clust[9:12])
+
+        # Number of days from the instance_clust
+        hours_day = 24
+        days_year = 365
+
+        @assert rep_hours%hours_day == 0 "Non-integer number of days"
+        num_days = rep_hours/hours_day |> Int
+        @assert num_days < days_year "More days than possible in an year"
+end
 
 @info "Creating output directories"
 if nosun
@@ -76,12 +98,25 @@ mkpath(output_path)
 results_dir = "results"
 mkpath(joinpath(output_path,results_dir))
 
-@info "Creating the energy system model"
+@info "Creating energy system model"
 (model, VariablesDict, ObjectivesDict) = EnergySystemModel(parameters, specs)
 # write_to_file(model,joinpath(output_path, model_dir,"model.mps"))
 
+if clust_method == "day"
+        @info "Adjusting storage levels and hydro reservoirs levels to rep. days"
+        T_sto = sort(vcat(((collect(2:num_days+1).-1)*hours_day),((collect(1:num_days-1)*hours_day).-1)))
+
+        @constraint(model,
+            s0_rd[s in parameters.S, n in parameters.N, t in T_sto],
+            model[:b_snt][s,n,t]/1000 == 0.5*model[:b̄_sn][s,n]/1000)
+
+        @constraint(model, 
+            h0_rd[h in parameters.H, n in parameters.N, t in T_sto],
+            model[:w_hnt][h,n,t]/1000 == parameters.Wmax_hn[h,n]/1000)
+end
+
 @info "Optimizing the model"
-optimizer = optimizer_with_attributes(Gurobi.Optimizer, "TimeLimit" => 60*60*72,
+optimizer = optimizer_with_attributes(Gurobi.Optimizer, "TimeLimit" => opt_tlim,
                                       "LogFile" => joinpath(output_path, "gurobi.log"))
 set_optimizer(model, optimizer)
 set_optimizer_attributes(model, "NumericFocus" => 1)
